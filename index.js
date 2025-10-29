e5bccef55b8652988abaac7fd5b0da24aef421f3
kazungu-p <patrickkazungu@icloud.com>
2025-10-29 12:21:51 +0300

    Added Email Transporter and Environment variables for Email Credentials

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-import express, { urlencoded } from "express";
+import express from "express";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth:{
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    }
+    
+});
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -21,9 +33,28 @@ app.get("/contact", (req, res)=>{
     res.render("contact.ejs");
 });
 
-app.post("/contact", (req, res)=>{
+app.post("/contact", async (req, res)=>{
     const {name, countryCode, number, email, subject, message} = req.body;
-    console.log("Form Submission: ", req.body);
-    res.send("Thank you! Your message has been received.")
+
+    const mailOptions = {
+        from:   email,
+        to: "patrickkazungu.me7@gmail.com",
+        subject:`New Contact from ${name}: ${subject}`,
+        text: `
+            Name:   ${name}
+            Phone:  +${countryCode}${number}
+            Email:  ${email}
+            
+            Message:
+            ${message}
+        `    
+    };
+    try{
+        await transporter.sendMail(mailOptions);
+        res.send("Your Message has been sent successfully!")
+    }catch(error){
+        console.error("Error sending email:", error);
+        res.status(500).send("Something went wrong. Please try again Later")
+    }
 });
 
